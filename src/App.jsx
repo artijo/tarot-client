5bb34b02a004d97c789242a2d822ecd13d1f40f0
ApichaiTi <81065469+artijo@users.noreply.github.com>
2024-08-17 21:43:34 +0700

    add tailwind and shandcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button>Shadcn UI Button</Button>
+      <p className='text-center'>Hello Tailwind</p>
     </>
   )
 }
