f4206e34071c5aa933803c3246bb275a66c21393
Pheeraphon12 <115926590+Pheeraphon12@users.noreply.github.com>
2024-09-23 14:21:18 +0700

    RegisterPage And Fix some login layout

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,40 +3,42 @@ import { Link } from "react-router-dom";
 
 function Login() {
     return (
-        <UserLayout>
-            <div class="max-w-4xl mx-auto">
-                <form class="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
-                    <div class="text-center mb-4 font-bold">
+        <>
+            <div className="flex items-center justify-center min-h-screen">
+                <form className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 border-2 w-full max-w-5xl">
+                    <div className="text-center mb-4 font-bold">
                         เข้าสู่ระบบ
                     </div>
-                    <div class="mb-4">
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    <div className="mb-4">
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                         id="username" type="text" placeholder="Username" />
                     </div>
 
-                    <div class="mb-6">
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+                    <div className="mb-6">
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
                         id="password" type="password" placeholder="Password" />
                     </div>
 
-                    <div class="flex items-center justify-between flex-col">
-                        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 w-full" 
-                        type="button">
-                            เข้าสู่ระบบ
-                        </button>
-                        <h3 class="flex items-center w-full mb-2">
-                            <span class="flex-grow bg-gray-200 rounded h-1"></span>
-                            <span class="mx-3 text-lg font-medium">หรือ</span>
-                            <span class="flex-grow bg-gray-200 rounded h-1"></span>
+                    <div className="flex items-center justify-between flex-col">
+                        <Link to="/" className="w-full">
+                            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 w-full" 
+                            type="button">
+                                เข้าสู่ระบบ
+                            </button>
+                        </Link>
+                        <h3 className="flex items-center w-full mb-2">
+                            <span className="flex-grow bg-gray-200 rounded h-1"></span>
+                            <span className="mx-3 text-lg font-medium">หรือ</span>
+                            <span className="flex-grow bg-gray-200 rounded h-1"></span>
                         </h3>
-                        <Link to="/register" class="w-full">
-                            <button class="bg-slate-700 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 w-full" 
+                        <Link to="/register" className="w-full">
+                            <button className="bg-slate-700 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 w-full" 
                                 type="button">
                                     สมัครสมาชิก
                             </button>
                         </Link>
-                        <a href="/" class="w-full">
-                            <button class="bg-slate-700 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 w-full" 
+                        <a href="/" className="w-full">
+                            <button className="bg-slate-700 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 w-full" 
                             type="button">
                                 ลืมรหัสผ่าน
                             </button>
@@ -44,7 +46,7 @@ function Login() {
                     </div>
                 </form>
             </div>
-        </UserLayout>
+        </>
     );
 }
 
