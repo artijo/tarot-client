0b9c9998530e2f696779a74a0f40ecd827b9ceb2
ggneteufa <ggnet880@gmail.com>
2024-09-24 11:48:24 +0700

    edit daily tarot page

--- src/Pages/Tarot_daily.jsx
+++ src/Pages/Tarot_daily.jsx
@@ -3,13 +3,48 @@ import UserLayout from "../Layouts/userLayout";
 
 function Tarot_daily() {
   const weeklylist = [
-    { name: "วันอาทิตย์" },
-    { name: "วันจันทร์" },
-    { name: "วันอังคาร" },
-    { name: "วันพุธ" },
-    { name: "วันพฤหัสบดี" },
-    { name: "วันศุกร์" },
-    { name: "วันเสาร์" },
+    {
+      name: "วันอาทิตย์",
+      class:
+        "text-center border border-1 rounded-full bg-[#f14c4c] w-36 h-36 pt-14",
+      link: "/",
+    },
+    {
+      name: "วันจันทร์",
+      class:
+        "text-center border border-1 rounded-full bg-[#ffeb3b] w-36 h-36 pt-14",
+      link: "/",
+    },
+    {
+      name: "วันอังคาร",
+      class:
+        "text-center border border-1 rounded-full bg-[#ee80b5] w-36 h-36 pt-14",
+      link: "/",
+    },
+    {
+      name: "วันพุธ",
+      class:
+        "text-center border border-1 rounded-full bg-[#8DBC51] w-36 h-36 pt-14",
+      link: "/",
+    },
+    {
+      name: "วันพฤหัสบดี",
+      class:
+        "text-center border border-1 rounded-full bg-[#FC8950] w-36 h-36 pt-14",
+      link: "/",
+    },
+    {
+      name: "วันศุกร์",
+      class:
+        "text-center border border-1 rounded-full bg-[#96C6FC] w-36 h-36 pt-14",
+      link: "/",
+    },
+    {
+      name: "วันเสาร์",
+      class:
+        "text-center border border-1 rounded-full bg-[#B47AE7] w-36 h-36 pt-14 col-start-2",
+      link: "/",
+    },
   ];
   console.log(weeklylist);
   return (
@@ -18,18 +53,18 @@ function Tarot_daily() {
         <h2 className="flex justify-center">ดวงรายวันตามวันเกิด</h2>
       </div>
 
-      <div className="flex justify-between">
-        {weeklylist.map((day, index) => (
-         
-            <Link
-              key={index}
-              to={"/"}
-              className="text-center border border-1 rounded-full p-10 bg-red-400"
-            >
+      <div className="flex justify-center grid grid-cols-3 gap-36 ms-32">
+        {weeklylist.map((day, index) =>
+          index === 6 ? (
+            <Link key={index} to={day.link} className={day.class}>
               {day.name}
             </Link>
-          
-        ))}
+          ) : (
+            <Link key={index} to={day.link} className={day.class}>
+              {day.name}
+            </Link>
+          )
+        )}
       </div>
     </UserLayout>
   );
