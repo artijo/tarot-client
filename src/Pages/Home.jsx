65dc1a857a840e6d4881969b0d62cb3d8e2288ea
Pheeraphon12 <115926590+Pheeraphon12@users.noreply.github.com>
2024-10-06 02:58:44 +0700

    check role and navigate admin to updatetarot

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,25 +5,38 @@ import Tarot_random from "./Tarot_random";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const { currentUser } = useAuth()
+  const [user_role, setRole] = useState(null)
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if(currentUser) {
+      axios.post('http://localhost:3000/auth/user',{email:currentUser.email})
+      .then((result)=>{
+          setRole(result.data[0].role);
+      })
+    }
+  },[])
   return (
     <UserLayout>
+      {currentUser && user_role === "admin" &&(
+        navigate("/updatetarot")
+      )}
       <h1 className="text-center text-white">ดูดวงด้วยไพ่ทาโร่</h1>
       <Tarot_random/>
       <div className="max-w-prose mx-auto space-y-7 text-white my-5">
-       
         {/* {currentUser && <p>สวัสดี!! คุณ "{currentUser.displayName}"</p>}         
         <p>เราเป็นกลุ่มเด็กไอทีที่รักการเขียนโค้ดไม่แพ้การเช็กดวง! ที่นี่คุณจะได้สัมผัสการดูดวงในแบบที่ไม่เหมือนใคร เพราะเราผสานศาสตร์แห่งดวงดาวกับเทคโนโลยีสุดล้ำ ไม่ว่าคุณจะเป็นสายมู สายเทค หรือสายชิลล์ ก็สามารถเช็กดวงง่าย ๆ ได้ทุกที่ทุกเวลา เพียงแค่ปลายนิ้ว ไม่ต้องพกยันต์หรือหมุนลูกเต๋าให้วุ่นวาย แค่คลิกเดียวก็รู้ได้เลยว่าวันนี้ดวงปังหรือพัง!</p> */}
-        {currentUser &&
+        {currentUser && user_role && (
           <div className="flex justify-center">
-            <button onClick={() => { doSignOut().then(() => { <Link to="/"></Link> }) }} 
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout
-            </button>
-          </div>
-        }
+          <button onClick={() => { doSignOut().then(() => { <Link to="/"></Link> }) }} 
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout
+          </button>
+        </div>
+        )}
       </div>
     </UserLayout>
   );
