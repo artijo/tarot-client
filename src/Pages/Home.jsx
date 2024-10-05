cf4bf684a6f6d7afd2256de1855c08e27813cd08
Pheeraphon12 <115926590+Pheeraphon12@users.noreply.github.com>
2024-10-05 17:17:40 +0700

    logout button & Hello user

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,25 @@
 import UserLayout from "../Layouts/userLayout";
 import { useAuth } from "../context/contextAuth";
 import { doSignOut } from "../firebase/firebase_login";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
   const { currentUser } = useAuth()
+
   return (
     <UserLayout>
       <div className="max-w-prose mx-auto space-y-7 text-white">
-        <h1 className="text-center">Tarot By DekIT</h1>        
+        <h1 className="text-center">Tarot By DekIT</h1>
+        {currentUser && <p>สวัสดี!! คุณ "{currentUser.displayName}"</p>}         
         <p>เราเป็นกลุ่มเด็กไอทีที่รักการเขียนโค้ดไม่แพ้การเช็กดวง! ที่นี่คุณจะได้สัมผัสการดูดวงในแบบที่ไม่เหมือนใคร เพราะเราผสานศาสตร์แห่งดวงดาวกับเทคโนโลยีสุดล้ำ ไม่ว่าคุณจะเป็นสายมู สายเทค หรือสายชิลล์ ก็สามารถเช็กดวงง่าย ๆ ได้ทุกที่ทุกเวลา เพียงแค่ปลายนิ้ว ไม่ต้องพกยันต์หรือหมุนลูกเต๋าให้วุ่นวาย แค่คลิกเดียวก็รู้ได้เลยว่าวันนี้ดวงปังหรือพัง!</p>
-        {currentUser && <p>{currentUser.displayName}</p>} 
-        {currentUser && <button onClick={() => { doSignOut().then(() => { navigate('/') }) }} className='text-sm text-blue-600 underline'>Logout</button>}
+        {currentUser &&
+          <div className="flex justify-center">
+            <button onClick={() => { doSignOut().then(() => { navigate('/') }) }} 
+              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout
+            </button>
+          </div>
+        }
       </div>
     </UserLayout>
   );
