821813f3db5bb98d49cd6d532810c9ef2e9d8fb5
Pheeraphon12 <115926590+Pheeraphon12@users.noreply.github.com>
2024-10-06 00:56:34 +0700

    Redirect User To Home if they enter "/updatetarot"

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import { useAuth } from "../context/contextAuth";
 import { doSignOut } from "../firebase/firebase_login";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Home() {
   const { currentUser } = useAuth()
@@ -15,7 +16,7 @@ function Home() {
         <p>เราเป็นกลุ่มเด็กไอทีที่รักการเขียนโค้ดไม่แพ้การเช็กดวง! ที่นี่คุณจะได้สัมผัสการดูดวงในแบบที่ไม่เหมือนใคร เพราะเราผสานศาสตร์แห่งดวงดาวกับเทคโนโลยีสุดล้ำ ไม่ว่าคุณจะเป็นสายมู สายเทค หรือสายชิลล์ ก็สามารถเช็กดวงง่าย ๆ ได้ทุกที่ทุกเวลา เพียงแค่ปลายนิ้ว ไม่ต้องพกยันต์หรือหมุนลูกเต๋าให้วุ่นวาย แค่คลิกเดียวก็รู้ได้เลยว่าวันนี้ดวงปังหรือพัง!</p>
         {currentUser &&
           <div className="flex justify-center">
-            <button onClick={() => { doSignOut().then(() => { navigate('/') }) }} 
+            <button onClick={() => { doSignOut().then(() => { <Link to="/"></Link> }) }} 
               className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout
             </button>
           </div>
--- src/Pages/Updatetarot.jsx
+++ src/Pages/Updatetarot.jsx
@@ -1,14 +1,38 @@
 import { useEffect, useState } from "react";
 import UserLayout from "../Layouts/userLayout";
 import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import { useAuth } from "../context/contextAuth";
+import { doSignOut } from "../firebase/firebase_login";
 
 function UpdateTarot(){
     const nav = [{name:"ดูดวงส่วนตัว",link:'/'}]
-
-    return(
-       <></>
-    )
-
-
+    const { currentUser } = useAuth()
+    const [user_role, setRole] = useState(null)
+    async function getUser() {
+            await axios.post('http://localhost:3000/auth/user',{email:currentUser.email})
+                    .then((result)=>{
+                        setRole(result.data[0].role);
+                    })
+        }
+    useEffect(()=>{
+        getUser()
+    },[])
+    if(user_role === "admin"){
+        return(
+            //Something here admin function
+            <>
+                <p>Hello Admin</p>
+            </>
+        )
+    }
+    else{
+        return(
+            setTimeout(()=>{
+                doSignOut().then(() => { window.location.href = "/" })
+            },1000),
+            alert("You are not Admin... Logging Out")
+        )
+    }
 }
 export default UpdateTarot;
