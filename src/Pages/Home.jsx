5911157aff2988caccf00c7b3764794b3d64368e
ApichaiTi <81065469+artijo@users.noreply.github.com>
2024-10-06 12:23:03 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -26,15 +26,15 @@ function Home() {
         navigate("/updatetarot")
       )}
       <h1 className="text-center text-white">ดูดวงด้วยไพ่ทาโร่</h1>
+        {currentUser && <p>สวัสดี!! คุณ "{currentUser.displayName}"</p>}         
+        <p className="text-white">เราเป็นกลุ่มเด็กไอทีที่รักการเขียนโค้ดไม่แพ้การเช็กดวง! ที่นี่คุณจะได้สัมผัสการดูดวงในแบบที่ไม่เหมือนใคร เพราะเราผสานศาสตร์แห่งดวงดาวกับเทคโนโลยีสุดล้ำ ไม่ว่าคุณจะเป็นสายมู สายเทค หรือสายชิลล์ ก็สามารถเช็กดวงง่าย ๆ ได้ทุกที่ทุกเวลา เพียงแค่ปลายนิ้ว ไม่ต้องพกยันต์หรือหมุนลูกเต๋าให้วุ่นวาย แค่คลิกเดียวก็รู้ได้เลยว่าวันนี้ดวงปังหรือพัง!</p>
       <Tarot_random/>
       <div className="max-w-prose mx-auto space-y-7 text-white my-5">
-        {/* {currentUser && <p>สวัสดี!! คุณ "{currentUser.displayName}"</p>}         
-        <p>เราเป็นกลุ่มเด็กไอทีที่รักการเขียนโค้ดไม่แพ้การเช็กดวง! ที่นี่คุณจะได้สัมผัสการดูดวงในแบบที่ไม่เหมือนใคร เพราะเราผสานศาสตร์แห่งดวงดาวกับเทคโนโลยีสุดล้ำ ไม่ว่าคุณจะเป็นสายมู สายเทค หรือสายชิลล์ ก็สามารถเช็กดวงง่าย ๆ ได้ทุกที่ทุกเวลา เพียงแค่ปลายนิ้ว ไม่ต้องพกยันต์หรือหมุนลูกเต๋าให้วุ่นวาย แค่คลิกเดียวก็รู้ได้เลยว่าวันนี้ดวงปังหรือพัง!</p> */}
         {currentUser && user_role && (
           <div className="flex justify-center">
-          <button onClick={() => { doSignOut().then(() => { <Link to="/"></Link> }) }} 
+          {/* <button onClick={() => { doSignOut().then(() => { <Link to="/"></Link> }) }} 
             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout
-          </button>
+          </button> */}
         </div>
         )}
       </div>
