c4f69739123ea06e7e5a3d176350a071890a7eb4
ApichaiTi <81065469+artijo@users.noreply.github.com>
2024-10-06 14:18:18 +0700

    Refactor adminLayout and HeaderAdmin components

---
+++ src/components/NavAdmin.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+const NavAdmin = ({ nav, path }) => {
+    return (
+      <>
+        {nav.map((item, index) => (
+          <Link
+            className="font-medium text-gray-500 transition hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 md:py-4"
+            to={item.link}
+            key={index}
+          >
+            <span
+            //   className={
+            //     path.startsWith(item.link)
+            //       ? 'font-bold text-gray-600 dark:text-gray-300'
+            //       : ''
+            //   }
+            >
+              {item.name}
+            </span>
+          </Link>
+        ))}
+      </>
+    );
+  };
+  
+export default NavAdmin;
